2bae994e42b942c8f232c58aa835e626297c5ffd
nattfeb <s160419080@student.ubaya.ac.id>
2022-04-07 20:38:13 +0700

    Min. E-Journal

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_160419080/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_160419080/view/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.ac.ubaya.informatika.anmp_uts_160419080.R
@@ -17,10 +20,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun showNavigation(){
         bottomNavigationView.visibility = View.VISIBLE
+        navView.visibility = View.VISIBLE
     }
 
     private fun hideNavigation(){
         bottomNavigationView.visibility = View.GONE
+//        navView.visibility = View.GONE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +33,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.loginFragment,
+//                R.id.registerFragment
+//            )
+//        )
+
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+
+//        setupActionBarWithNavController(navController!!, appBarConfiguration!!)
 
         bottomNavigationView.setupWithNavController(navController)
 
@@ -38,6 +55,10 @@ class MainActivity : AppCompatActivity() {
                 else -> showNavigation()
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,drawerLayout) || super.onSupportNavigateUp()
     }
 
 
