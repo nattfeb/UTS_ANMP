4e7c20b3c7cea06bafc675faca5ac2a42d12ff6d
nattfeb <s160419080@student.ubaya.ac.id>
2022-04-07 19:42:12 +0700

    Min. SideNav and Ejournal 2

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_160419080/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_160419080/view/MainActivity.kt
@@ -4,17 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.ac.ubaya.informatika.anmp_uts_160419080.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private fun showNavigation(){
+        bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideNavigation(){
+        bottomNavigationView.visibility = View.GONE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.loginFragment -> hideNavigation()
+                R.id.registerFragment -> hideNavigation()
+                else -> showNavigation()
+            }
+        }
     }
+
 
 
 }
